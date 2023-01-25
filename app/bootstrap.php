e6021b65a2c3e65780b8d48230eb216320a72997
Abraham Emmanuel <sundayemmanuelabraham@gmail.com>
2023-01-25 14:29:31 +0100

    completed controllers

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,8 @@
-bootstrap
+<?php
+//Load Libraries
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
+
+//Init Core Library
+$init = new Core;
---
+++ app/controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+class Pages 
+{
+    public function __construct()
+    {
+
+    }
+
+	public function index()
+	{
+		echo 'INDEX';
+	}
+	
+	public function about($id)
+	{
+		echo $id;
+	}
+}
